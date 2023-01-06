ef6a4984a009f2ff88bd8aac139789ce5d1f6203
lksads <lksadsii@gmail.com>
2023-01-06 17:04:17 -0300

    banco de dados

---
+++ Dump20230106.sql
@@ -0,0 +1,382 @@
+-- MySQL dump 10.13  Distrib 5.7.17, for Win64 (x86_64)
+--
+-- Host: 127.0.0.1    Database: agendador
+-- ------------------------------------------------------
+-- Server version	5.5.5-10.4.24-MariaDB
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `tb_agendamento`
+--
+
+DROP TABLE IF EXISTS `tb_agendamento`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `tb_agendamento` (
+  `id_agendamento` int(11) NOT NULL AUTO_INCREMENT,
+  `horario_inicio` datetime DEFAULT NULL,
+  `horario_fim` datetime DEFAULT NULL,
+  `recorrente` tinyint(1) DEFAULT NULL,
+  `id_pessoa_evento` int(11) DEFAULT NULL,
+  `id_sala` int(11) DEFAULT NULL,
+  `id_departamento` int(11) DEFAULT NULL,
+  `id_usuario_agendamento` int(11) DEFAULT NULL,
+  `observacao` varchar(200) DEFAULT NULL,
+  PRIMARY KEY (`id_agendamento`),
+  KEY `id_pessoa_evento` (`id_pessoa_evento`),
+  KEY `id_sala` (`id_sala`),
+  KEY `id_departamento` (`id_departamento`),
+  KEY `id_usuario_agendamento` (`id_usuario_agendamento`),
+  CONSTRAINT `tb_agendamento_ibfk_1` FOREIGN KEY (`id_pessoa_evento`) REFERENCES `tb_pessoa` (`id_pessoa`),
+  CONSTRAINT `tb_agendamento_ibfk_2` FOREIGN KEY (`id_sala`) REFERENCES `tb_sala` (`id_sala`),
+  CONSTRAINT `tb_agendamento_ibfk_3` FOREIGN KEY (`id_departamento`) REFERENCES `tb_departamento` (`id_departamento`),
+  CONSTRAINT `tb_agendamento_ibfk_4` FOREIGN KEY (`id_usuario_agendamento`) REFERENCES `tb_usuario` (`id_usuario`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tb_agendamento`
+--
+
+LOCK TABLES `tb_agendamento` WRITE;
+/*!40000 ALTER TABLE `tb_agendamento` DISABLE KEYS */;
+/*!40000 ALTER TABLE `tb_agendamento` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `tb_departamento`
+--
+
+DROP TABLE IF EXISTS `tb_departamento`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `tb_departamento` (
+  `id_departamento` int(11) NOT NULL,
+  `nome_departamento` varchar(200) DEFAULT NULL,
+  PRIMARY KEY (`id_departamento`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tb_departamento`
+--
+
+LOCK TABLES `tb_departamento` WRITE;
+/*!40000 ALTER TABLE `tb_departamento` DISABLE KEYS */;
+INSERT INTO `tb_departamento` VALUES (0,'ATI');
+/*!40000 ALTER TABLE `tb_departamento` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `tb_nivel_acesso`
+--
+
+DROP TABLE IF EXISTS `tb_nivel_acesso`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `tb_nivel_acesso` (
+  `id_nivel_acesso` int(11) NOT NULL,
+  `descricao` varchar(200) DEFAULT NULL,
+  PRIMARY KEY (`id_nivel_acesso`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tb_nivel_acesso`
+--
+
+LOCK TABLES `tb_nivel_acesso` WRITE;
+/*!40000 ALTER TABLE `tb_nivel_acesso` DISABLE KEYS */;
+INSERT INTO `tb_nivel_acesso` VALUES (0,'ADM');
+/*!40000 ALTER TABLE `tb_nivel_acesso` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `tb_pessoa`
+--
+
+DROP TABLE IF EXISTS `tb_pessoa`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `tb_pessoa` (
+  `id_pessoa` int(11) NOT NULL,
+  `nome_pessoa` varchar(200) DEFAULT NULL,
+  `id_nivel_acesso` int(11) NOT NULL AUTO_INCREMENT,
+  PRIMARY KEY (`id_pessoa`),
+  KEY `tb_pessoa_ibfk_1` (`id_nivel_acesso`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tb_pessoa`
+--
+
+LOCK TABLES `tb_pessoa` WRITE;
+/*!40000 ALTER TABLE `tb_pessoa` DISABLE KEYS */;
+INSERT INTO `tb_pessoa` VALUES (0,'kygo',1),(1,'jojo',1);
+/*!40000 ALTER TABLE `tb_pessoa` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `tb_sala`
+--
+
+DROP TABLE IF EXISTS `tb_sala`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `tb_sala` (
+  `id_sala` int(11) NOT NULL,
+  `nome_sala` varchar(200) DEFAULT NULL,
+  PRIMARY KEY (`id_sala`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tb_sala`
+--
+
+LOCK TABLES `tb_sala` WRITE;
+/*!40000 ALTER TABLE `tb_sala` DISABLE KEYS */;
+INSERT INTO `tb_sala` VALUES (0,'sala01');
+/*!40000 ALTER TABLE `tb_sala` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `tb_usuario`
+--
+
+DROP TABLE IF EXISTS `tb_usuario`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `tb_usuario` (
+  `id_usuario` int(11) NOT NULL,
+  `login` varchar(40) DEFAULT NULL,
+  `id_pessoa` int(11) DEFAULT NULL,
+  `id_nivel_acesso` int(11) DEFAULT NULL,
+  `id_departamento` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id_usuario`),
+  KEY `id_nivel_acesso` (`id_nivel_acesso`),
+  KEY `id_pessoa` (`id_pessoa`),
+  CONSTRAINT `tb_usuario_ibfk_1` FOREIGN KEY (`id_pessoa`) REFERENCES `tb_pessoa` (`id_pessoa`),
+  CONSTRAINT `tb_usuario_ibfk_2` FOREIGN KEY (`id_nivel_acesso`) REFERENCES `tb_nivel_acesso` (`id_nivel_acesso`),
+  CONSTRAINT `tb_usuario_ibfk_3` FOREIGN KEY (`id_pessoa`) REFERENCES `tb_pessoa` (`id_pessoa`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tb_usuario`
+--
+
+LOCK TABLES `tb_usuario` WRITE;
+/*!40000 ALTER TABLE `tb_usuario` DISABLE KEYS */;
+INSERT INTO `tb_usuario` VALUES (0,'adm',0,0,0);
+/*!40000 ALTER TABLE `tb_usuario` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2023-01-06 14:54:51
+-- MySQL dump 10.13  Distrib 5.7.17, for Win64 (x86_64)
+--
+-- Host: 127.0.0.1    Database: db_agendador
+-- ------------------------------------------------------
+-- Server version	5.5.5-10.4.24-MariaDB
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `tb_agendamento`
+--
+
+DROP TABLE IF EXISTS `tb_agendamento`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `tb_agendamento` (
+  `id_agendamento` int(11) NOT NULL AUTO_INCREMENT,
+  `horario_inicio` datetime DEFAULT NULL,
+  `horario_fim` datetime DEFAULT NULL,
+  `fk_pessoa` int(11) DEFAULT NULL,
+  `fk_sala` int(11) DEFAULT NULL,
+  `fk_departamento` int(11) DEFAULT NULL,
+  `observacao` varchar(400) DEFAULT NULL,
+  PRIMARY KEY (`id_agendamento`),
+  KEY `fk_sala_idx` (`fk_sala`),
+  KEY `fk_departamento_idx` (`fk_departamento`),
+  KEY `fk_pessoa_idx` (`fk_pessoa`),
+  CONSTRAINT `departamento` FOREIGN KEY (`fk_departamento`) REFERENCES `tb_departamento` (`id_departamento`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `pessoa` FOREIGN KEY (`fk_pessoa`) REFERENCES `tb_pessoa` (`id_pessoa`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `sala` FOREIGN KEY (`fk_sala`) REFERENCES `tb_sala` (`id_sala`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tb_agendamento`
+--
+
+LOCK TABLES `tb_agendamento` WRITE;
+/*!40000 ALTER TABLE `tb_agendamento` DISABLE KEYS */;
+/*!40000 ALTER TABLE `tb_agendamento` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `tb_departamento`
+--
+
+DROP TABLE IF EXISTS `tb_departamento`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `tb_departamento` (
+  `id_departamento` int(11) NOT NULL AUTO_INCREMENT,
+  `nome_departamento` varchar(200) DEFAULT NULL,
+  PRIMARY KEY (`id_departamento`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tb_departamento`
+--
+
+LOCK TABLES `tb_departamento` WRITE;
+/*!40000 ALTER TABLE `tb_departamento` DISABLE KEYS */;
+/*!40000 ALTER TABLE `tb_departamento` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `tb_nivel_acesso`
+--
+
+DROP TABLE IF EXISTS `tb_nivel_acesso`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `tb_nivel_acesso` (
+  `id_nivel_acesso` int(11) NOT NULL AUTO_INCREMENT,
+  `descricao` varchar(200) DEFAULT NULL,
+  PRIMARY KEY (`id_nivel_acesso`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tb_nivel_acesso`
+--
+
+LOCK TABLES `tb_nivel_acesso` WRITE;
+/*!40000 ALTER TABLE `tb_nivel_acesso` DISABLE KEYS */;
+/*!40000 ALTER TABLE `tb_nivel_acesso` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `tb_pessoa`
+--
+
+DROP TABLE IF EXISTS `tb_pessoa`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `tb_pessoa` (
+  `id_pessoa` int(11) NOT NULL AUTO_INCREMENT,
+  `nome_pessoa` varchar(200) DEFAULT NULL,
+  `fk_nivel_acesso` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id_pessoa`),
+  KEY `fk_nivel_acesso_idx` (`fk_nivel_acesso`),
+  CONSTRAINT `fk_nivel_acesso` FOREIGN KEY (`fk_nivel_acesso`) REFERENCES `tb_nivel_acesso` (`id_nivel_acesso`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tb_pessoa`
+--
+
+LOCK TABLES `tb_pessoa` WRITE;
+/*!40000 ALTER TABLE `tb_pessoa` DISABLE KEYS */;
+/*!40000 ALTER TABLE `tb_pessoa` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `tb_sala`
+--
+
+DROP TABLE IF EXISTS `tb_sala`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `tb_sala` (
+  `id_sala` int(11) NOT NULL AUTO_INCREMENT,
+  `nome_sala` varchar(200) DEFAULT NULL,
+  PRIMARY KEY (`id_sala`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tb_sala`
+--
+
+LOCK TABLES `tb_sala` WRITE;
+/*!40000 ALTER TABLE `tb_sala` DISABLE KEYS */;
+/*!40000 ALTER TABLE `tb_sala` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `tb_usuario`
+--
+
+DROP TABLE IF EXISTS `tb_usuario`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `tb_usuario` (
+  `id_usuario` int(11) NOT NULL AUTO_INCREMENT,
+  `login` varchar(45) DEFAULT NULL,
+  `fk_pessoa` int(11) DEFAULT NULL,
+  `fk_nivel_acesso` int(11) DEFAULT NULL,
+  `fk_departamento` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id_usuario`),
+  KEY `fk_pessoa_idx` (`fk_pessoa`),
+  KEY `fk_departamento_idx` (`fk_departamento`),
+  KEY `fk_nivel_acesso_idx` (`fk_nivel_acesso`),
+  CONSTRAINT `fk_departamento` FOREIGN KEY (`fk_departamento`) REFERENCES `tb_departamento` (`id_departamento`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `fk_pessoa` FOREIGN KEY (`fk_pessoa`) REFERENCES `tb_pessoa` (`id_pessoa`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tb_usuario`
+--
+
+LOCK TABLES `tb_usuario` WRITE;
+/*!40000 ALTER TABLE `tb_usuario` DISABLE KEYS */;
+/*!40000 ALTER TABLE `tb_usuario` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2023-01-06 14:54:51
